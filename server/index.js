de0c7b8a1dbea2ce5b8e035f6c3056459cbfe5f5
Ruby Lee <rubymleee@gmail.com>
2018-10-11 19:45:08 -0700

    proxy

--- server/index.js
+++ server/index.js
@@ -1,22 +1,25 @@
+require('newrelic');
 const express = require('express');
 const app = express();
+const proxy = require('express-http-proxy');
 
-const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const compression = require('compression');
 
 const router = require('./routes.js');
 
-app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(compression());
 
-app.use('/:id', express.static('public'));
+app.use('/:nameOrId', express.static('public'));
 
-app.set('port', process.env.PORT || 8080);
+app.set('port', 8080);
 
-app.use('/api', router);
+app.get('/api/sidebar/restaurants/:nameOrId', proxy('http://ec2-52-53-240-118.us-west-1.compute.amazonaws.com'));
+// app.get('/api/sidebar/restaurants/:nameOrId', proxy('http://localhost:7878/'));
 
+// app.use('http://localhost:7878', router);
+// app.use('/api', router);
 app.listen(app.get('port'), () => {
   console.log(`app is listening to port ${app.get('port')}`);
 });
